4f88ada3d89b9a325f346899b790613be69a42f7
syl319@hotmail.com <syl319@hotmail.com>
2024-04-08 18:17:59 -0400

    Login and Signup Error

--- EntertainLog/Models/ViewModel/LoginViewModel.cs
+++ EntertainLog/Models/ViewModel/LoginViewModel.cs
@@ -2,13 +2,17 @@
 
 namespace EntertainLog.Models.ViewModel
 {
+    /// <summary>
+    /// Created By: Yuanlong Song
+    /// The structure holding the objects required by the Login info
+    /// </summary>
     public class LoginViewModel
     {
         [Required(ErrorMessage = "Username is required")]
-        public string Username { get; set; }
+        public string? Username { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
         [DataType(DataType.Password)]
-        public string Password { get; set; }
+        public string? Password { get; set; }
     }
 }
