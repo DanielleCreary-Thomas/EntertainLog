276339be1d5a524bdb4f5c35347a48846fe77eb0
syl319@hotmail.com <syl319@hotmail.com>
2024-04-08 22:42:56 -0400

    Commited before pull

--- EntertainLog/obj/Debug/net8.0/EntertainLog.AssemblyInfo.cs
+++ EntertainLog/obj/Debug/net8.0/EntertainLog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntertainLog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6765f32413f445f89975b2a602cd314d96ea14e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f558f68175ea463000bd434d7cb6d3f318a69e91")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntertainLog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntertainLog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
